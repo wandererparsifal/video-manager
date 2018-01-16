8dd079fc2870f8f5cea7f0f6e9668dde38b80560
wandererparsifal <wandererparsifal@gmail.com>
2018-01-16 16:52:12 +0800

    Grids动态生产图片式样

--- src/renderer/components/Grids.vue
+++ src/renderer/components/Grids.vue
@@ -1,28 +1,7 @@
 <template>
   <div>
     <div class="wrapper">
-      <div class="box a">
-        <img src="../assets/logo.png" class="image">
-      </div>
-      <div class="box b">
-        <img src="../assets/logo.png" class="image">
-      </div>
-      <div class="box c">
-        <img src="../assets/logo.png" class="image">
-      </div>
-      <div class="box d">
-        <img src="../assets/logo.png" class="image">
-      </div>
-      <div class="zoomImage e" :style="'background-image: url(../../static/290580.jpg)'">
-        <img src="../assets/logo.png" class="image">
-      </div>
-      <div class="zoomImage f" :style="'background-image: url(../../static/forest-3840x2400-sunset-fox-4k-15517.jpg)'">
-        <img src="../assets/logo.png" class="image">
-      </div>
-      <div class="box g">
-        <img src="../assets/logo.png" class="image">
-      </div>
-      <div class="box h">
+      <div v-for="item in items" :key="item" :style="item">
         <img src="../assets/logo.png" class="image">
       </div>
     </div>
@@ -32,10 +11,40 @@
 <script>
   import { ipcRenderer } from 'electron'; // eslint-disable-line
 
+  const style = 'background-color: @color@;' +
+    'grid-column-start: @column-start@;' +
+    'grid-column-end: @column-end@;' +
+    'grid-row-start: @row-start@;' +
+    'grid-row-end: @row-end@;' +
+    'background-image: url(@image@);' +
+    'background-size: cover;' +
+    'text-align: center;';
+
+  function createStyle(color, columnStart, columnEnd, rowStart, rowEnd, image) {
+    return style.replace('@color@', color)
+      .replace('@column-start@', columnStart)
+      .replace('@column-end@', columnEnd)
+      .replace('@row-start@', rowStart)
+      .replace('@row-end@', rowEnd)
+      .replace('@image@', image);
+  }
+
+  function getColRow(index, colNum) {
+    const array = [];
+    array.push(((index % colNum) * 2) + 1);
+    array.push(((index % colNum) * 2) + 2);
+    array.push((parseInt(index / colNum, 10) * 2) + 1);
+    console.log((parseInt(index / colNum, 10) * 2) + 1);
+    array.push((parseInt(index / colNum, 10) * 2) + 2);
+    console.log((parseInt(index / colNum, 10) * 2) + 2);
+    return array;
+  }
+
   export default {
     name: 'grids',
     data() {
       return {
+        items: [],
         msg: '',
       };
     },
@@ -49,12 +58,16 @@
         console.log(otherData);
         this.msg = otherData;
       });
+
+      for (let i = 0; i < 12; i += 1) {
+        const array = getColRow(i, 4);
+        this.items.push(createStyle('#404', array[0], array[1], array[2], array[3], '../../static/556062.jpg'));
+      }
     },
   };
 </script>
 
 <style>
-
   .wrapper {
     background-color: #f0f9eb;
     display: grid;
@@ -63,73 +76,8 @@
     grid-template-rows: 20vh 1vh 20vh 1vh 20vh;
   }
 
-  .box {
-    background-color: #444;
-    text-align: center;
-  }
-
-  .a {
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 1;
-    grid-row-end: 2;
-  }
-
-  .b {
-    grid-column-start: 3;
-    grid-column-end: 4;
-    grid-row-start: 1;
-    grid-row-end: 2;
-  }
-
-  .c {
-    grid-column-start: 5;
-    grid-column-end: 6;
-    grid-row-start: 1;
-    grid-row-end: 2;
-  }
-
-  .d {
-    grid-column-start: 7;
-    grid-column-end: 8;
-    grid-row-start: 1;
-    grid-row-end: 2;
-  }
-
-  .e {
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 3;
-    grid-row-end: 4;
-  }
-
-  .f {
-    grid-column-start: 3;
-    grid-column-end: 4;
-    grid-row-start: 3;
-    grid-row-end: 4;
-  }
-
-  .g {
-    grid-column-start: 5;
-    grid-column-end: 6;
-    grid-row-start: 3;
-    grid-row-end: 4;
-  }
-
-  .h {
-    grid-column-start: 7;
-    grid-column-end: 8;
-    grid-row-start: 3;
-    grid-row-end: 4;
-  }
-
   .image {
     width: 100%;
     height: 100%;
-  }
-
-  .zoomImage {
-    background-size: cover
   }
 </style>
